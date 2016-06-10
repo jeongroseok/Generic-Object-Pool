5ae3809192f9eb110eaf19fcce67f6a3b626b581
Seok Jeong-ro <jeongroseok@gmail.com>
2016-06-10 15:42:27 +0900

    First Commit

---
+++ Pool.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pooling
+{
+    public class Pool<T>
+        where T : class
+    {
+        public interface IObjectHandler
+        {
+            T Create();
+            void Destory(T poolObject);
+            void Initialize(T poolObject);
+            void Deinitialize(T poolObject);
+        }
+
+        public class PoolEventArgs : System.EventArgs
+        {
+            public T Value { get; private set; }
+            public PoolEventArgs(T value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private IObjectHandler objectHandler;
+        private Queue<T> queue = new Queue<T>();
+        //private List<T> managed = new List<T>();
+
+        public event EventHandler<PoolEventArgs> Created;
+        public event EventHandler<PoolEventArgs> Initialized;
+        public event EventHandler<PoolEventArgs> Deinitialized;
+
+        public Pool(IObjectHandler objectHandler, int reserve = 10)
+        {
+            this.objectHandler = objectHandler;
+            Reserve(reserve);
+        }
+
+        public void Reserve(int reserve)
+        {
+            if (queue.Count >= reserve)
+            {
+                return;
+            }
+
+            int d = reserve - queue.Count;
+            for (int i = 0; i < d; i++)
+            {
+                Create();
+            }
+        }
+
+        public T Pull()
+        {
+            if (queue.Count <= 0)
+            {
+                Create();
+            }
+            var obj = queue.Dequeue();
+            objectHandler.Initialize(obj);
+            if (Initialized != null)
+            {
+                Initialized(this, new PoolEventArgs(obj));
+            }
+            return obj;
+        }
+
+        public void Push(T instance)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            if (queue.Contains(instance))
+            {
+                throw new ArgumentException("duplicated", "instance");
+            }
+            objectHandler.Deinitialize(instance);
+            if (Deinitialized != null)
+            {
+                Deinitialized(this, new PoolEventArgs(instance));
+            }
+            queue.Enqueue(instance);
+        }
+
+        public void Clear()
+        {
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                objectHandler.Destory(current);
+            }
+        }
+
+        private void Create()
+        {
+            var o = objectHandler.Create();
+            queue.Enqueue(o);
+            if (Created != null)
+            {
+                Created(this, new PoolEventArgs(o));
+            }
+            //managed.Add(o);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,12 +4,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Generic_Object_Pool
+namespace Pooling
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Pool<Fruit> pool = new Pool<Fruit>(new FruitHandler(), 3);
+            pool.Created += (sender, eventArgs) =>
+            {
+                Console.WriteLine(eventArgs.Value + "가 생성되었다.");
+            };
+            pool.Initialized += (sender, eventArgs) =>
+            {
+                Console.WriteLine(eventArgs.Value + "가 초기화 되었다.");
+            };
+            pool.Deinitialized += (sender, eventArgs) =>
+            {
+                Console.WriteLine(eventArgs.Value + "가 해제되었다.");
+            };
+
+            var fruit = pool.Pull();
+            pool.Push(fruit);
+
+            Queue<Fruit> q = new Queue<Fruit>();
+            for (int i = 0; i < 5; i++)
+            {
+                q.Enqueue(pool.Pull());
+            }
+
+            while (q.Count > 0)
+            {
+                pool.Push(q.Dequeue());
+            }
+
+            pool.Clear();
+        }
+    }
+
+    class FruitHandler : Pool<Fruit>.IObjectHandler
+    {
+        public Fruit Create()
+        {
+            var obj = new Fruit("사과");
+            Console.WriteLine(obj.ToString() + " 생성");
+            return obj;
+        }
+
+        public void Initialize(Fruit poolObject)
+        {
+            Console.WriteLine(poolObject.ToString() + " 초기화");
+        }
+
+        public void Deinitialize(Fruit poolObject)
+        {
+            Console.WriteLine(poolObject.ToString() + " 해제");
+        }
+
+        public void Destory(Fruit poolObject)
+        {
+            Console.WriteLine(poolObject.ToString() + " 파괴");
+        }
+    }
+
+    class Fruit
+    {
+        private static int idCount = 0;
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+
+        public Fruit(string name)
+        {
+            this.ID = idCount++;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}번 {1}]", ID, Name);
         }
     }
 }
